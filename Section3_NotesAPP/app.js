74ccc26ec4ba551d34c6b8b0ce80cffb31eb89f3
Logan Wood <lwood3499imac@logans-imac.attlocal.net>
2017-07-05 16:23:47 -0500

    working on noteapp

---
+++ Section3_NotesAPP/app.js
@@ -0,0 +1,7 @@
+
+console.log('Starting app.js');
+
+const fs = require('fs'); // fetch all contents of the fs module and store it in the constant called 'fs'
+const _ = require('lodash'); // fetch the 3rd party module to use in this file (must have it installed in package first)
+
+const notes = require('./notes.js');
