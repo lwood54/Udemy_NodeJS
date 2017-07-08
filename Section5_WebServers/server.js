228b5b01562062506a1f5fd9e40ee7d3e9c2c23e
Logan Wood <lwood3499imac@logans-imac.attlocal.net>
2017-07-08 18:27:13 -0500

    using template tags with handlebars.js and express

--- Section5_WebServers/server.js
+++ Section5_WebServers/server.js
@@ -1,30 +1,34 @@
 const express = require('express');
+const hbs = require('hbs');
 
-// to create an app, just call the method
 var app = express();
 
-// setting up our http route handlers
-// here we are registering our handler
-// this is the '/' route, aka the 'root' route
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+// hbs.registerHelper() takes 2 arguments, the name of the argument, and the function to run
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'Logan',
-        likes: [
-            'programming',
-            'woodworking',
-            'Cowboys'
-        ]
+    res.render('home.hbs', {
+        pageTitle: "Mr. Wood's Science Page!",
+        welcomeMessage: "Yeah science!!!"
     });
 });
 
-// setting up a 2nd page, simply call app.get() again.
-// make sure to use '/', but after that you can identify it how you want.
 app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>');
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
 });
 
-// setting a 3rd page, for handling failed requests.
 app.get('/bad', (req, res) => {
     res.send({
         statuscode: 404,
@@ -32,4 +36,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000); // common port for development
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
